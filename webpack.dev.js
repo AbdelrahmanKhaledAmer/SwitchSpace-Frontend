249e4a41c83e03e372694fd81ec21db2d63672d9
mohamedazab <mohamedazab1996@gmail.com>
2020-06-08 22:24:40 +0200

    style: enforce style and syntax

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,16 +1,15 @@
 "use strict";
 
-const path   = require('path');
-const merge  = require('webpack-merge');
+const path = require("path");
+const merge = require("webpack-merge");
 
-const common = require('./webpack.common.js');
-
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: path.resolve(__dirname,'dist'),
-        compress: true,
-        port: 8000
-    }
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    compress: true,
+    port: 8000,
+  },
 });
