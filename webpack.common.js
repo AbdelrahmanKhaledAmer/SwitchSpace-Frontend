0244354768e74c3677b23c10a668fe7c0e654285
mohamedazab <mohamedazab1996@gmail.com>
2020-06-20 16:46:10 +0200

    feat: add support for file and image compilation

--- webpack.common.js
+++ webpack.common.js
@@ -47,6 +47,10 @@ module.exports = {
           use: "css-loader",
         }),
       },
+      {
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        loader: "url-loader?limit=100000",
+      },
     ],
   },
   plugins: [
