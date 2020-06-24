f6fb48a01ab087584eaef90b2034614b15cf6847
Mostafa Shorim <mostafa.shorim@sap.com>
2020-06-24 02:31:27 +0200

    including UserProfileView component in App routes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import {
 import UserLoginView from "./views/UserLoginView";
 import UserSignupView from "./views/UserSignupView";
 import TrendingView from "./views/TrendingView";
-
+import UserProfileView from "./views/UserProfileView";
 export default class App extends React.Component {
     constructor(props) {
         super(props);
@@ -22,6 +22,7 @@ export default class App extends React.Component {
                 {component: UserLoginView, path: "/login"},
                 {component: UserSignupView, path: "/register"},
                 {component: TrendingView, path: "/trending"},
+                {component: UserProfileView, path: "/profile/:id"},
             ],
         };
     }
