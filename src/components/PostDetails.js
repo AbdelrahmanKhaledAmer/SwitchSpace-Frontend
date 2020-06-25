1aa67fd212c3adbfa484ee8dbfffa6f440757e89
mohamedAlmorsi <mohamed.almorsi.alshaer@gmail.com>
2020-06-25 17:23:02 +0200

    Added initial dependencies

---
+++ src/components/PostDetails.js
@@ -0,0 +1,16 @@
+"use strict";
+
+import React from "react";
+import {withStyles} from "@material-ui/core/styles";
+import PropTypes from "prop-types";
+import Card from "@material-ui/core/Card";
+import Page from "./Page";
+
+const styles = theme => ({});
+
+class PostDetails extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+}
+export default withStyles(styles)(PostDetails);
