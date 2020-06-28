5ac420a03449fbdb9f78dc1204516146b701644b
mohamedazab <mohamedazab1996@gmail.com>
2020-06-28 17:13:23 +0200

    feat: services admin service

---
+++ src/services/AdminService.js
@@ -0,0 +1,61 @@
+// router.get("/report", logger, middlewares.checkAuthentication, reportController.viewAllReports);
+// router.delete("/report", logger, middlewares.checkAuthentication, reportController.deleteReport);
+// router.delete("/post", logger, middlewares.checkAuthentication, postController.remove);
+"use strict";
+import HttpService from "./HttpService";
+
+export default class AdminService {
+    constructor() {}
+    static baseURL() {
+        return "/moderate/";
+    }
+
+    static getReports() {
+        return new Promise((resolve, reject) => {
+            HttpService.get(
+                `${AdminService.baseURL()}report`,
+                function (data) {
+                    //resolve
+                    resolve(data);
+                },
+                function (textStatus) {
+                    //reject
+                    reject(textStatus);
+                }
+            );
+        });
+    }
+
+    static deleteReports(id) {
+        return new Promise((resolve, reject) => {
+            HttpService.remove(
+                `${AdminService.baseURL()}report`,
+                id,
+                function (data) {
+                    //resolve
+                    resolve(data);
+                },
+                function (textStatus) {
+                    //reject
+                    reject(textStatus);
+                }
+            );
+        });
+    }
+    static deletePost(id) {
+        return new Promise((resolve, reject) => {
+            HttpService.remove(
+                `${AdminService.baseURL()}post`,
+                id,
+                function (data) {
+                    //resolve
+                    resolve(data);
+                },
+                function (textStatus) {
+                    //reject
+                    reject(textStatus);
+                }
+            );
+        });
+    }
+}
