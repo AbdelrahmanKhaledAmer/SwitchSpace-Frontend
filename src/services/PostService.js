6c56639ee1926fd78a9c81e2d0a1d58ed6845bef
Mostafa Shorim <mostafa.shorim@sap.com>
2020-06-28 15:52:37 +0200

    implementing getUserPosts in PostService

---
+++ src/services/PostService.js
@@ -0,0 +1,25 @@
+"use strict";
+
+import HttpService from "./HttpService";
+
+export default class PostService {
+    constructor() {}
+
+    static baseURL() {
+        return "/post";
+    }
+
+    static getUserPosts(userId) {
+        return new Promise((resolve, reject) => {
+            HttpService.get(
+                `${PostService.baseURL()}?userId=${userId}`,
+                function (data) {
+                    resolve(data);
+                },
+                function (textStatus) {
+                    reject(textStatus);
+                }
+            );
+        });
+    }
+}
