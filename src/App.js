a7e12aaa1865ce25ac8b649788734fcfbed9e2f4
mohamedazab <mohamedazab1996@gmail.com>
2020-06-29 22:05:17 +0200

    add: admin views in app

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ import TrendingView from "./views/TrendingView";
 import PostView from "./views/PostView";
 import SubscriptionsView from "./views/SubscriptonsView";
 import SearchFilterView from "./views/SearchFilterView";
+import AdminLoginView from "./views/AdminLoginView";
+import AdminView from "./views/AdminView";
 
 import UserProfileView from "./views/UserProfileView";
 export default class App extends React.Component {
@@ -23,12 +25,17 @@ export default class App extends React.Component {
         this.state = {
             title: "Switch Space",
             routes: [
+                {component: AdminLoginView, path: "/admin/auth"},
+                // TODO: check loggedin Admin
+                {component: AdminView, path: "/admin/reports"},
                 {component: UserLoginView, path: "/login"},
                 {component: UserSignupView, path: "/register"},
                 {component: SearchFilterView, path: "/search"},
                 {component: TrendingView, path: "/trending"},
                 {component: PostView, path: "/post"},
+                // TODO: check loggedin
                 {component: UserProfileView, path: "/profile/:id"},
+                // TODO: check loggedin
                 {component: SubscriptionsView, path: "/charge"},
             ],
         };
