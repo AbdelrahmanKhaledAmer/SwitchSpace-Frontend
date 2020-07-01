de9e5bab798ed8f3e0d7e598c8058c1717c54753
mohamedazab <mohamedazab1996@gmail.com>
2020-07-01 10:59:39 +0200

    feat: finish info card integration in map

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -6,14 +6,24 @@ import PropTypes from "prop-types";
 // import Card from "@material-ui/core/Card";
 // import Paper from "@material-ui/core/Paper";
 // import CardContent from "@material-ui/core/Card";
-import Typography from "@material-ui/core/Typography";
 import {Grid} from "@material-ui/core";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import InfoCard from "./InfoCard";
+import SwapHorizOutlinedIcon from "@material-ui/icons/SwapHorizOutlined";
 
+//TODO: loading styles
+const LoadingContainer = () => {
+    return <CircularProgress size={100} />;
+};
 // infowindow style
 const styles = () => ({
-    root: {
-        minWidth: "50vw",
-        maxHeight: "50vh",
+    infoWindowContainer: {
+        // need min width to prevent collapsing
+        minWidth: 300,
+        // minHeight: 240,
+        // needed for wrapping text
+        maxWidth: 420,
+        maxHeight: 420,
     },
     bullet: {
         display: "inline-block",
@@ -88,14 +98,14 @@ class GoogleMap extends React.Component {
         this.props.onLocationChange(position);
     }
     // open infowindow when marker is clicked
-    onMarkerClicked(props, marker, e) {
+    onMarkerClicked(props, marker) {
         // console.log(props.name);
-        console.log(e);
-        console.log(marker);
-        console.log(props);
-        this.setState({showInfo: true, activePost: {idx: marker.name, marker: marker}});
+        const idx = marker.name;
+        // post is stored in activePost because it is needed for the map
+        this.setState({showInfo: true, activePost: {idx: idx, post: this.props.posts[idx], marker: marker}});
         this.props.onPostFocusChange(marker.name);
     }
+
     // triggered when the map finishes loading
     mapLoaded(mapProps, map) {
         console.log("map loaded");
@@ -117,92 +127,82 @@ class GoogleMap extends React.Component {
         const {classes} = this.props;
         console.log(this.props.posts);
         return (
-            <div>
-                <Map
-                    google={this.props.google}
-                    onReady={(mapProps, map) => this.mapLoaded(mapProps, map)}
-                    onClick={this.onMapClicked}
-                    zoom={11}
-                    center={{lat: this.state.mapCenterLat, lng: this.state.mapCenterLong}}>
-                    {this.props.posts.map((post, idx) => (
-                        <Marker
-                            key={idx}
-                            title={post._id}
-                            name={idx}
-                            position={{lat: post.exchangeLocation.coordinates[1], lng: post.exchangeLocation.coordinates[0]}}
-                            onClick={this.onMarkerClicked}
-                            // icon={{
-                            //     url: "https://switchspace-datastore.s3.eu-central-1.amazonaws.com/clipdealer_A51141491_preview.jpg",
-                            //     anchor: new this.props.google.maps.Point(32, 32),
-                            //     scaledSize: new this.props.google.maps.Size(32, 32),
-                            // }}
-                        />
-                    ))}
+            <Map
+                google={this.props.google}
+                onReady={(mapProps, map) => this.mapLoaded(mapProps, map)}
+                onClick={this.onMapClicked}
+                zoom={11}
+                center={{lat: this.state.mapCenterLat, lng: this.state.mapCenterLong}}>
+                {this.props.posts.map((post, idx) => (
+                    //TODO: ICONS
                     <Marker
-                        key={"myLoc"}
-                        title={this.state.myMarker.title}
-                        name={this.state.myMarker.name}
-                        position={this.state.myMarker.position}
-                        // onClick={this.onMarkerClicked}
-                        icon={{
-                            url: "https://switchspace-datastore.s3.eu-central-1.amazonaws.com/clipdealer_A51141491_preview.jpg",
-                            anchor: new this.props.google.maps.Point(32, 32),
-                            scaledSize: new this.props.google.maps.Size(32, 32),
-                        }}
+                        key={idx}
+                        title={post._id}
+                        name={idx}
+                        position={{lat: post.exchangeLocation.coordinates[1], lng: post.exchangeLocation.coordinates[0]}}
+                        onClick={this.onMarkerClicked}
+                        // icon={{
+                        //     url: "https://switchspace-datastore.s3.eu-central-1.amazonaws.com/clipdealer_A51141491_preview.jpg",
+                        //     anchor: new this.props.google.maps.Point(32, 32),
+                        //     scaledSize: new this.props.google.maps.Size(32, 32),
+                        // }}
                     />
-                    <InfoWindow
-                        visible={this.state.showInfo}
-                        styles={{width: "550px", height: "250px"}}
-                        marker={this.state.activePost ? this.state.activePost.marker : undefined}>
-                        {/* <CardContent> */}
+                ))}
+                <Marker
+                    key={"myLoc"}
+                    title={this.state.myMarker.title}
+                    name={this.state.myMarker.name}
+                    position={this.state.myMarker.position}
+                    // onClick={this.onMarkerClicked}
+                    //TODO: ICONS
+                    icon={{
+                        url: "https://switchspace-datastore.s3.eu-central-1.amazonaws.com/clipdealer_A51141491_preview.jpg",
+                        anchor: new this.props.google.maps.Point(32, 32),
+                        scaledSize: new this.props.google.maps.Size(32, 32),
+                    }}
+                />
+                <InfoWindow
+                    visible={this.state.showInfo}
+                    styles={{textAlign: "center"}}
+                    marker={this.state.activePost ? this.state.activePost.marker : undefined}>
+                    {/* <CardContent> */}
 
-                        <Grid container spacing={2} className={classes.root}>
-                            <Grid item xs={6}>
-                                <Typography className={classes.title} color="textSecondary" gutterBottom>
-                                    {"Item Owned"}
-                                </Typography>
-                                <Typography variant="h5" component="h2">
-                                    {this.state.activePost ? this.props.posts[this.state.activePost.idx].itemOwned.title : ""}
-                                </Typography>
-                                <Typography className={classes.pos} color="textSecondary">
-                                    {this.state.activePost ? this.props.posts[this.state.activePost.idx].itemOwned.category : ""}
-                                </Typography>
-                                <Typography variant="body1" component="p">
-                                    {this.state.activePost ? this.props.posts[this.state.activePost.idx].itemOwned.condition : ""}
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={6}>
-                                <Typography className={classes.title} color="textSecondary" gutterBottom>
-                                    {"Item Desired"}
-                                </Typography>
-                                <Typography variant="h5" component="h2">
-                                    {this.state.activePost ? this.props.posts[this.state.activePost.idx].itemDesired.title : ""}
-                                </Typography>
-                                <Typography className={classes.pos} color="textSecondary">
-                                    {this.state.activePost ? this.props.posts[this.state.activePost.idx].itemDesired.category : ""}
-                                </Typography>
-                                <Typography variant="body1" component="p">
-                                    {this.state.activePost ? this.props.posts[this.state.activePost.idx].itemOwned.condition : ""}
-                                </Typography>
-                            </Grid>
+                    <Grid container justify="space-between" alignItems="center" className={classes.infoWindowContainer}>
+                        <Grid item>
+                            <InfoCard
+                                header={this.state.activePost ? "Item Owned" : ""}
+                                title={this.state.activePost ? this.state.activePost.post.itemOwned.title : ""}
+                                infoBig={this.state.activePost ? this.state.activePost.post.itemOwned.category : ""}
+                                infoSmall={this.state.activePost ? this.state.activePost.post.itemOwned.condition : ""}
+                            />
                         </Grid>
-                        {/* </CardContent> */}
-                    </InfoWindow>
+                        <Grid item>
+                            <SwapHorizOutlinedIcon fontSize="inherit" />
+                        </Grid>
+                        <Grid item>
+                            <InfoCard
+                                header={this.state.activePost ? "Item Desired" : ""}
+                                title={this.state.activePost ? this.state.activePost.post.itemDesired.title : ""}
+                                infoBig={this.state.activePost ? this.state.activePost.post.itemDesired.category : ""}
+                                infoSmall={this.state.activePost ? this.state.activePost.post.itemDesired.condition : ""}
+                            />
+                        </Grid>
+                    </Grid>
+                </InfoWindow>
 
-                    <Circle
-                        radius={this.props.radius ? this.props.radius : 10000}
-                        center={this.state.myMarker.position}
-                        // onMouseover={() => console.log("mouseover")}
-                        onClick={this.onMapClicked}
-                        // onMouseout={() => console.log("mouseout")}
-                        strokeColor="transparent"
-                        strokeOpacity={0}
-                        strokeWeight={5}
-                        fillColor="blue"
-                        fillOpacity={0.3}
-                    />
-                </Map>
-            </div>
+                <Circle
+                    radius={this.props.radius ? this.props.radius : 10000}
+                    center={this.state.myMarker.position}
+                    // onMouseover={() => console.log("mouseover")}
+                    onClick={this.onMapClicked}
+                    // onMouseout={() => console.log("mouseout")}
+                    strokeColor="transparent"
+                    strokeOpacity={0}
+                    strokeWeight={5}
+                    fillColor="blue"
+                    fillOpacity={0.3}
+                />
+            </Map>
         );
     }
 }
@@ -210,5 +210,6 @@ class GoogleMap extends React.Component {
 export default withStyles(styles)(
     GoogleApiWrapper({
         // apiKey: "API key here",
+        LoadingContainer: LoadingContainer,
     })(GoogleMap)
 );
