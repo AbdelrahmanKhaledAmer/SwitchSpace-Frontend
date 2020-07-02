10f5ab2a7ea061c428df082c72d44946662842be
mohamedazab <mohamedazab1996@gmail.com>
2020-07-02 10:51:22 +0200

    feat: global loading in profile

--- src/views/UserProfileView.js
+++ src/views/UserProfileView.js
@@ -2,32 +2,20 @@
 // React
 import React from "react";
 import PropTypes from "prop-types";
-// Material UI Core
-import CircularProgress from "@material-ui/core/CircularProgress";
-import {withStyles} from "@material-ui/core/styles";
 // Components
+import Loading from "../components/Loading";
 import UserProfile from "../components/UserProfile/UserProfile";
 // Services
 import PostService from "../services/PostService";
 import UserService from "../services/UserService";
 import UserAuthService from "../services/UserAuthService";
 
-const styles = {
-    centered: {
-        position: "fixed",
-        top: "50%",
-        left: "50%",
-        marginLeft: "-50px",
-        marginTop: "-50px",
-    },
-};
-
-class UserProfileView extends React.Component {
+export default class UserProfileView extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            pageLoading: true,
+            loading: true,
             selectedTab: "posts",
             userId: this.props.match.params.id,
             tabs: [
@@ -40,9 +28,10 @@ class UserProfileView extends React.Component {
                     value: "reviews",
                 },
             ],
+            posts: [],
             isMyProfile: false,
-            myEmail: undefined,
-            userInfo: {},
+            myEmail: "",
+            userInfo: {name: "", commRate: 0, descriptionRate: 0, conditionRate: 0, profilePicture: {}, reviews: []},
         };
 
         this.handleTabChange = this.handleTabChange.bind(this);
@@ -51,16 +40,17 @@ class UserProfileView extends React.Component {
 
     static get propTypes() {
         return {
-            classes: PropTypes.object.isRequired,
             match: PropTypes.object.isRequired,
         };
     }
 
     async componentDidMount() {
+        // get current user
         const currentUser = UserAuthService.getCurrentUser();
         const tabs = this.state.tabs;
         let isMyProfile = false;
-        if (currentUser.id == this.state.userId) {
+        // my profile, not admin
+        if (!currentUser.isAdmin && currentUser.id == this.state.userId) {
             isMyProfile = true;
             tabs.push({
                 label: "Settings",
@@ -72,7 +62,7 @@ class UserProfileView extends React.Component {
             const userInfoResp = await UserService.getUserInfo(this.state.userId);
 
             this.setState({
-                pageLoading: false,
+                loading: false,
                 userInfo: userInfoResp.data.data,
                 posts: postsResp.data.data,
                 isMyProfile: isMyProfile,
@@ -85,6 +75,8 @@ class UserProfileView extends React.Component {
         }
     }
     async updateProfile(user) {
+        // TODO: reload and try get another token with the new username
+        this.setState({loading: true});
         const data = new FormData();
         for (let key in user) {
             data.append(key, user[key]);
@@ -93,11 +85,11 @@ class UserProfileView extends React.Component {
             await UserService.updateProfile(data);
 
             //TODO: notify and update tab
-            this.setState({selectedTab: "posts"});
         } catch (err) {
             //TODO: handle notification
             console.log(err);
         }
+        this.setState({loading: false});
     }
 
     handleTabChange(event, newValue) {
@@ -107,30 +99,20 @@ class UserProfileView extends React.Component {
     }
 
     render() {
-        const {classes} = this.props;
-
-        // TODO: use a common loader for all components
-        if (this.state.pageLoading) {
-            return (
-                <div>
-                    <CircularProgress size={100} className={classes.centered} />
-                </div>
-            );
-        }
-
         return (
-            <UserProfile
-                tabs={this.state.tabs}
-                isMyProfile={this.state.isMyProfile}
-                myEmail={this.state.myEmail}
-                selectedTab={this.state.selectedTab}
-                onTabChange={this.handleTabChange}
-                onProfileUpdate={this.updateProfile}
-                userInfo={this.state.userInfo}
-                posts={this.state.posts}
-            />
+            <div>
+                <Loading loading={this.state.loading} />
+                <UserProfile
+                    tabs={this.state.tabs}
+                    isMyProfile={this.state.isMyProfile}
+                    myEmail={this.state.myEmail}
+                    selectedTab={this.state.selectedTab}
+                    onTabChange={this.handleTabChange}
+                    onProfileUpdate={this.updateProfile}
+                    userInfo={this.state.userInfo}
+                    posts={this.state.posts}
+                />
+            </div>
         );
     }
 }
-
-export default withStyles(styles)(UserProfileView);
