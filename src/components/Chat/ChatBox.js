c635be2da389d1de08ceb62610c6b8b1528b4790
Mostafa Shorim <mostafa.shorim@sap.com>
2020-07-07 04:46:52 +0200

    implementing ChatBox

--- src/components/Chat/ChatBox.js
+++ src/components/Chat/ChatBox.js
@@ -2,62 +2,107 @@
 // React
 import React from "react";
 import PropTypes from "prop-types";
+//Material UI Core
 import {withStyles} from "@material-ui/core/styles";
-// Chat
-import {ChatFeed, Message} from "react-chat-ui";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import TextField from "@material-ui/core/TextField";
+// Material UI Icons
+import CloseIcon from "@material-ui/icons/Close";
+import SendIcon from "@material-ui/icons/Send";
+// chat-react-elements
+import {MessageList} from "react-chat-elements";
+import "react-chat-elements/dist/main.css";
 
 const styles = theme => ({
-    top: {
-        marginTop: theme.spacing(20),
+    container: {
+        display: "flex",
+        flexDirection: "column",
+        width: "350px",
+        height: "450px",
+        position: "fixed",
+        bottom: 0,
+        right: theme.spacing(8),
+        zIndex: 10,
     },
-    chatContainer: {
-        width: "200px",
-        height: "200px",
+    header: {
+        background: theme.palette.type === "dark" ? theme.palette.primary.dark : theme.palette.primary.light,
+        height: "50px",
+    },
+    headerAvatar: {
+        marginLeft: theme.spacing(1),
+    },
+    messageList: {
+        flex: 1,
+        overflow: "auto",
+        marginTop: theme.spacing(2),
+    },
+    messageInput: {
+        marginLeft: theme.spacing(1),
     },
 });
 
 class ChatBox extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            messages: [
-                new Message({
-                    id: 1,
-                    message: "I'm the recipient! (The person you're talking to)",
-                }), // Gray bubble
-                new Message({id: 0, message: "I'm you -- the blue bubble!"}), // Blue bubble
-            ],
-        };
     }
 
     static get propTypes() {
         return {
             classes: PropTypes.object.isRequired,
+            otherUserPicture: PropTypes.object.isRequired,
+            otherUserName: PropTypes.string.isRequired,
+            messages: PropTypes.array.isRequired,
+            closeChat: PropTypes.func.isRequired,
+            sendMessage: PropTypes.func.isRequired,
+            onMessageInputChange: PropTypes.func.isRequired,
+            messageInput: PropTypes.string.isRequired,
         };
     }
 
     render() {
         const {classes} = this.props;
         return (
-            <div className={classes.chatContainer}>
-                <ChatFeed
-                    messages={this.state.messages} // Boolean: list of message objects
-                    isTyping={this.state.is_typing} // Boolean: is the recipient typing
-                    hasInputField={false} // Boolean: use our input, or use your own
-                    showSenderName // show the name of the user who sent the message
-                    bubblesCentered={false} //Boolean should the bubbles be centered in the feed?
-                    // JSON: Custom bubble styles
-                    bubbleStyles={{
-                        text: {
-                            fontSize: 18,
-                        },
-                        chatbubble: {
-                            borderRadius: 70,
-                            padding: 40,
-                        },
-                    }}
-                />
-            </div>
+            <Paper className={classes.container} elevation={5}>
+                <Paper className={classes.header} elevation={5}>
+                    <Grid container>
+                        <Grid item container xs spacing={1} alignItems="center">
+                            <Grid item>
+                                <Avatar className={classes.headerAvatar} src={this.props.otherUserPicture ? this.props.otherUserPicture.url : null} />
+                            </Grid>
+                            <Grid item>
+                                <Typography color="inherit"> {this.props.otherUserName}</Typography>
+                            </Grid>
+                        </Grid>
+                        <Grid item>
+                            <IconButton onClick={this.props.closeChat}>
+                                <CloseIcon />
+                            </IconButton>
+                        </Grid>
+                    </Grid>
+                </Paper>
+                <MessageList className={classes.messageList} toBottomHeight={"100%"} dataSource={this.props.messages} />
+                <Grid container>
+                    <Grid item xs={10}>
+                        <TextField
+                            className={classes.messageInput}
+                            fullWidth
+                            multiline={true}
+                            value={this.props.messageInput}
+                            placeholder="Type your message here..."
+                            onChange={this.props.onMessageInputChange}
+                        />
+                    </Grid>
+                    <Grid item xs={2}>
+                        <IconButton onClick={this.props.sendMessage}>
+                            <SendIcon />
+                        </IconButton>
+                    </Grid>
+                </Grid>
+            </Paper>
         );
     }
 }
