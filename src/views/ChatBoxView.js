25ef66c4808afd88d3012ded67373a2306d8b318
Mostafa Shorim <mostafa.shorim@sap.com>
2020-07-11 04:36:46 +0200

    opening chat box in case a chat is selected from chat menu

--- src/views/ChatBoxView.js
+++ src/views/ChatBoxView.js
@@ -48,8 +48,10 @@ export default class ChatBoxView extends React.Component {
 
     static get propTypes() {
         return {
-            // chatReceiverId is not a required prop
-            chatReceiverId: PropTypes.string,
+            // receiverIdFromPost is not a required prop
+            receiverIdFromPost: PropTypes.string,
+            receiverIdFromMenu: PropTypes.string.isRequired,
+            setUnreadMessages: PropTypes.func.isRequired,
         };
     }
 
@@ -68,8 +70,12 @@ export default class ChatBoxView extends React.Component {
     }
 
     componentDidUpdate(prevProps) {
-        if (this.props.chatReceiverId && this.props.chatReceiverId !== prevProps.chatReceiverId) {
-            this.getChatHistory(this.props.chatReceiverId);
+        if (this.props.receiverIdFromPost && this.props.receiverIdFromPost !== prevProps.receiverIdFromPost) {
+            this.getChatHistory(this.props.receiverIdFromPost);
+        }
+
+        if (this.props.receiverIdFromMenu && this.props.receiverIdFromMenu !== prevProps.receiverIdFromMenu) {
+            this.getChatHistory(this.props.receiverIdFromMenu);
         }
     }
 
@@ -137,6 +143,7 @@ export default class ChatBoxView extends React.Component {
                 otherUserPicture: chatHistory.otherUserPicture,
                 messages: messagesFormatted,
             });
+            this.props.setUnreadMessages();
         } catch (err) {
             this.notify(err, "error");
         }
