934e344237728b316ca199cc0271c2c858ca9a71
mohamedAlmorsi <mohamed.almorsi.alshaer@gmail.com>
2020-07-15 14:05:56 +0200

    Edited 404 not found page

--- src/components/NotFound404.js
+++ src/components/NotFound404.js
@@ -3,19 +3,52 @@
 import React from "react";
 import PropTypes from "prop-types";
 import {withStyles} from "@material-ui/core/styles";
+import {withRouter} from "react-router-dom";
+
+// React components
+import CardHeader from "@material-ui/core/CardHeader";
+import Button from "@material-ui/core/Button";
+
 // Components
 import Page from "./Page";
-//source image
-import pageNotFound from "../../public/assets/general/404.png";
 
-const styles = {
+const styles = theme => ({
     center: {
         height: "70%",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        position: "absolute",
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        margin: "auto",
     },
-};
+    center1: {
+        height: "70%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        position: "absolute",
+        bottom: 0,
+        left: 0,
+        right: 0,
+        margin: "auto",
+    },
+    cardHeader: {
+        backgroundColor: theme.palette.button.error,
+        color: theme.palette.button.textColor,
+    },
+    button: {
+        top: 1,
+        backgroundColor: theme.palette.button.backgroundColor(),
+        color: theme.palette.button.textColor(),
+        "&:hover": {
+            backgroundColor: theme.palette.button.hover.backgroundColor(),
+        },
+    },
+});
 
 class NotFound404 extends React.Component {
     constructor(props) {
@@ -25,6 +58,7 @@ class NotFound404 extends React.Component {
     static get propTypes() {
         return {
             classes: PropTypes.object.isRequired,
+            history: PropTypes.object.isRequired,
         };
     }
 
@@ -33,10 +67,21 @@ class NotFound404 extends React.Component {
         return (
             <Page>
                 <div className={classes.center}>
-                    <img src={pageNotFound} />
+                    <CardHeader title="404 We Could't find what you are looking for" className={classes.cardHeader} />
+                </div>
+                <div className={classes.center1}>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={() => {
+                            this.props.history.push("/");
+                        }}
+                        className={classes.button}>
+                        Home
+                    </Button>
                 </div>
             </Page>
         );
     }
 }
-export default withStyles(styles)(NotFound404);
+export default withRouter(withStyles(styles)(NotFound404));
