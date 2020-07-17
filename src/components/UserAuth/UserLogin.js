7d2ba398001b791b2416e724bc666f87151aa3bd
mohamedazab <mohamedazab1996@gmail.com>
2020-07-17 17:19:05 +0200

    fix: remove unused vars

--- src/components/UserAuth/UserLogin.js
+++ src/components/UserAuth/UserLogin.js
@@ -7,8 +7,6 @@ import {Link} from "react-router-dom";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
 import Typography from "@material-ui/core/Typography";
 import {withStyles} from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
